95ee2bd3d037e7ed05056c0b685e7c313375d3f0
Elshen000 <elshen.q@itbrains.az>
2023-04-11 00:16:56 +0400

    Add project files.

---
+++ CatalogService/Controllers/CatalogController.cs
@@ -0,0 +1,26 @@
+﻿using CatalogService.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        DatabaseContext _db;
+        public CatalogController(DatabaseContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _db.Products.ToList();
+        }
+    }
+}
